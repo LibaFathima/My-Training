9023692e58e401ad2e00a5609c18bce967414e7d
ROSE <ROSE@ADMIN>
2023-03-02 12:26:04 +0530

    First Commit

---
+++ My_Maven/src/main/java/xlread/XlCode.java
@@ -0,0 +1,41 @@
+package xlread;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class XlCode {
+	public static FileInputStream f;
+	public static XSSFWorkbook w;
+	public static XSSFSheet s;
+public static String readStringData(int i,int j) throws IOException
+{
+	f=new FileInputStream("C:\\Users\\ROSE\\Desktop\\Excelread.xlsx");
+	w=new XSSFWorkbook(f);
+	s=w.getSheet("Sheet1");
+	Row r=s.getRow(i);
+	Cell c=r.getCell(j);
+	return c.getStringCellValue();
+}
+	
+public static String readIntData(int i,int j) throws IOException 
+{ 
+	f=new FileInputStream("C:\\Users\\ROSE\\Desktop\\Excelread.xlsx");
+w=new XSSFWorkbook(f);
+s=w.getSheet("Sheet1");
+Row r=s.getRow(i);
+Cell c=r.getCell(j);
+int a= (int) c.getNumericCellValue();
+return String.valueOf(a);
+
+
+	
+}
+}
+
+     
---
+++ My_Maven/src/main/java/xlread/XlMain.java
@@ -0,0 +1,14 @@
+package xlread;
+
+import java.io.IOException;
+
+public class XlMain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+System.out.println(XlCode.readStringData(0,1));
+System.out.println(XlCode.readIntData(0,0));
+	}
+
+}
